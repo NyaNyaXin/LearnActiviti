15fe09adfb0d1d6772a7e8615ed953abd49d6ddd
nyac <nyac@DESKTOP-6MRJ08S>
2018-12-06 22:23:09 +0800

    创建任务与绑定候选用户   以及查询  指定用户有哪些任务

---
+++ test-task/src/com/cx/act/CandidateTest.java
@@ -0,0 +1,34 @@
+package com.cx.act;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.activiti.engine.IdentityService;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.identity.User;
+import org.activiti.engine.task.Task;
+
+public class CandidateTest {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ProcessEngine engine = ProcessEngines.getDefaultProcessEngine();
+		TaskService taskService = engine.getTaskService();
+		IdentityService identityService = engine.getIdentityService();
+		User user = identityService.newUser(UUID.randomUUID().toString());
+		user.setFirstName("测试用户");
+		identityService.saveUser(user);
+		Task task= taskService.newTask(UUID.randomUUID().toString());
+		task.setName("测试任务");
+		taskService.saveTask(task);
+		taskService.addCandidateUser(task.getId(), user.getId());
+		
+		List<Task> list = taskService.createTaskQuery().taskCandidateUser(user.getId()).list();
+		for(Task t:list) {
+			System.out.println(t.getName());
+		}
+	}
+}
